82e399a2b1b2c0a308a5d3896f5e6ad477d43cc2
James Westby <james.westby@canonical.com>
2010-10-01 13:19:36 -0400

    Release 0.6.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 if __name__ == '__main__':
     setup(name="bzr-builder",
-          version="0.4",
+          version="0.6",
           description="Turn a recipe in to a bzr branch",
           author="James Westby",
           author_email="james.westby@canonical.com",
