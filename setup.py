a5ced6df04c7b79415815157dcdd930551e6b73d
Jelmer Vernooij <jelmer@samba.org>
2011-11-10 18:32:04 +0100

    Add info file.

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
+from info import *
+
 from distutils.core import setup
 
 if __name__ == '__main__':
+    version = bzr_plugin_version[:3]
+    version_string = ".".join([str(x) for x in version])
+
     setup(name="bzr-builder",
-          version="0.7.2",
+          version=version_string,
           description="Turn a recipe in to a bzr branch",
           author="James Westby",
           author_email="james.westby@canonical.com",
